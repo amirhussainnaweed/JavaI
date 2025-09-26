f88f59e353434fe3d9b473ac94365787e56018e2
Amir Hussain <amirhussainnaweed@gmail.com>
2025-09-26 18:34:22 +0430

    Add files via upload

---
+++ Display_time.java
@@ -0,0 +1,17 @@
+package seconds_to_minutes;
+
+import java.util.Scanner;
+
+public class Seconds_to_minutes {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+       System.out.println("Enter the amount of seconds to convert into minutes and seconds:");
+       double total_seconds = input.nextDouble();
+       double total_minutes = total_seconds / 60;
+       double remaining_seconds = total_seconds % 60;
+       System.out.println("This much seconds is equal to " + (int)total_minutes + " minutes and " + (int)remaining_seconds + " seconds.");
+     }
+ }
