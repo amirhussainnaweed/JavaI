6df5c5deec77010babbd73d7b9fd6a4d7e9a8782
Amir Hussain <amirhussainnaweed@gmail.com>
2025-09-26 23:49:49 +0430

    Add files via upload

---
+++ Simple_math_learning_tool.java
@@ -0,0 +1,24 @@
+package checking_number_division_on_2_and_3;
+
+import java.util.Scanner;
+
+public class Checking_number_division_on_2_and_3 {
+
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+int a = (int)(Math.random() * 10);
+        int b = (int)(Math.random() * 10);
+
+        int result = a + b;
+
+        System.out.println("What is the result of " + a + "+" + b);
+        int user_result = input.nextInt();
+
+        if (result == user_result)
+            System.out.println("Well done! Great!");
+        else
+            System.out.println("Wrong answer! Try again!");
+
+    }
+
+}
